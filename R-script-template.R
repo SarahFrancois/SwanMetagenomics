d6d462784b7dc4dc05032bdb94d3349516a9bb36
SarahFrancois <sarah.francois@zoo.ox.ac.uk>
2019-06-18 09:57:02 +0100

    2019-06-18

--- R-script-template.R
+++ R-script-template.R
@@ -1,1 +1,8 @@
 #R-file Template
+
+#Set the working directory ----
+setwd("/Users/user/Desktop/Bioinfo-Stat-Tools/R-stats-scripts-tuto/Data-synthesis-and-visualisation")
+
+# Libraries
+
+#Data loading
